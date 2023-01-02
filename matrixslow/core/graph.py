5c5fbd30b2f46245b684425f9244f7f72d66421d
Jiayi Ni <nijiayi1119@outlook.com>
2023-01-02 14:24:59 -0800

    Implemented the computation and jacobian for perception loss

---
+++ matrixslow/core/graph.py
@@ -0,0 +1,5 @@
+class Graph:
+    pass
+
+
+default_graph = Graph()
--- matrixslow/ops/loss.py
+++ matrixslow/ops/loss.py
@@ -7,4 +7,10 @@ class LossFunction(Node):
 
 
 class PerceptionLoss(LossFunction):
-    pass
+    def compute(self):
+        self.value = np.mat(
+            np.where(self.parents[0].value >= 0.0, 0.0, -self.parents[0].value))
+
+    def get_jacobi(self, parent):
+        diag = np.where(parent.value >= 0.0, 0.0, -1)
+        return np.diag(diag.ravel())
